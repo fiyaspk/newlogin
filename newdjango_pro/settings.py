3514ec6afd37942008de043ceee7e12d540ac297
fiyas <fiyaspk1820@gmail.com>
2021-04-12 12:26:00 +0530

    first commit

--- newdjango_pro/settings.py
+++ newdjango_pro/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
